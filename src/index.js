e34de04a36a52125cc009c5e542b9c12402afe5a
Samarths12 <0312sammyy@gmail.com>
2024-11-30 18:40:45 +0530

    final update ig

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
